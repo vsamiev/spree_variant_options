4243b1a3b0350dc7a7c6a834dc67f2b58313cad3
Manuel Vidaurre <manuel.vidaurre@agiltec.com.mx>
2014-10-26 14:40:07 -0600

    Shows colors variants images at home and shows the appropiate variant on click

---
+++ app/controllers/spree/products_controller_decorator.rb
@@ -0,0 +1,22 @@
+module ProductExtensions
+  extend ActiveSupport::Concern
+
+  included do
+    singleton_class.prepend ClassMethods
+    prepend InstanceMethods
+  end
+
+  module ClassMethods
+
+  end
+
+  module InstanceMethods
+    def show
+      super
+      @color = params[:color]
+    end
+  end
+
+end
+
+Spree::ProductsController.include ProductExtensions

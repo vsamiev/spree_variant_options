0bd8cbc7ef979b580fb0a6f113e9a1924857f299
Manuel Vidaurre <manuel.vidaurre@agiltec.com.mx>
2014-10-26 14:38:07 -0600

    FIX #1 correcting the update of main image

--- app/assets/javascripts/spree/frontend/product_variant_options.js
+++ app/assets/javascripts/spree/frontend/product_variant_options.js
@@ -13,7 +13,7 @@ var show_variant_images = function(variants_ids) {
     // hide it and select the first available thumb instead.
     if(!currentThumb.hasClass('vtmb-' + variant_id)) {
       //var thumb = $($('ul.thumbnails li:visible').eq(0));
-      var thumb = $($("ul.thumbnails li.vtmb-" + variant_id + ":first").eq(0));
+      var thumb = $($("ul.thumbnails li.tmb-" + variant_id + ":first").eq(0));
       if (thumb.length == 0) {
         thumb = $($('ul.thumbnails li:visible').eq(0));
       }
